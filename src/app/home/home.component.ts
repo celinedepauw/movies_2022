285808870c1926adf2b9c66589d103ac7877f5c5
celin <celin@localhost>
2022-01-12 14:31:35 +0100

    home component template ok

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  search!: FormControl;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.search = new FormControl('');
+  }
+
+  onSubmit(){
+    console.log('search = ' + this.search.value)
   }
 
 }
