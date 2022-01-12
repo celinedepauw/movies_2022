3fec2a0a07f19fdd395c8467275da3672b60b7de
celin <celin@localhost>
2022-01-12 11:10:03 +0100

    validators in login component and link to home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,14 +9,17 @@ import { MatInputModule } from '@angular/material/input';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     FlexLayoutModule,
     FormsModule,
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      login: new FormControl('', [Validators.required, Validators.pattern('celine@hotmail.com')]),
+      password: new FormControl('', [Validators.required, Validators.pattern('cinema')])
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.value.login == 'celine@hotmail.com' && this.loginForm.value.password == 'cinema'){
+      this.router.navigateByUrl('/home')
+    }
   }
 
 }
