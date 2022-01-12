2135dc9d59c212aada2c51782112d4803800ba19
celin <celin@localhost>
2022-01-12 14:42:04 +0100

    new component for movie details

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { DetailsMovieComponent } from './details-movie/details-movie.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    HomeComponent
+    HomeComponent,
+    DetailsMovieComponent
   ],
   imports: [
     BrowserModule,
